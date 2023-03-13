ea04c05323b9c78abf5ffc046914aa3b3adb8369
Pelusa <agustinlorenzatto@gmail.com>
2023-03-13 09:27:01 -0300

    Haciendo un poco mejor el tema de lista de precios

--- sistemaFacturacion/src/productos/cambiarPrecios.js
+++ sistemaFacturacion/src/productos/cambiarPrecios.js
@@ -46,9 +46,12 @@ archivo.addEventListener('change',e=>{
         let lista = [];
         let listaNueva = [];
         for await(let dato of datos){
-            const producto = productos.find(elem=>elem.cod_fabrica === dato.Id);
-            producto && lista.push(producto);
+            const productoBuscado = productos.find(elem=>elem.cod_fabrica === dato.Id);
+            const producto = productoBuscado && JSON.parse(JSON.stringify(productoBuscado));
+            producto && lista.push(productoBuscado);
             producto && (producto.costo = dato["Precio Vigente"]);
+            const impuesto = producto ? parseFloat(producto.costo) + (parseFloat(producto.costo) * parseFloat(producto.impuestos) / 100) : 0;
+            producto && (producto.precio_venta = impuesto + (parseFloat(producto.utilidad) * impuesto / 100));
             producto &&  listaNueva.push(producto);
         }
         llenarListaVieja(lista);
@@ -59,21 +62,25 @@ archivo.addEventListener('change',e=>{
 
 const llenarListaVieja = async(lista)=>{
     for(let elem of lista){
+        console.log(elem)
         const tr = document.createElement('tr');
 
         const tdCodigo = document.createElement('td');
         const tdDescripcion = document.createElement('td');
         const tdCostoViejo = document.createElement('td');
+        const tdCostoDolaresViejo = document.createElement('td');
         const tdPrecioViejo = document.createElement('td');
 
         tdCodigo.innerHTML = elem._id;
         tdDescripcion.innerHTML = elem.descripcion;
-        tdCostoViejo.innerHTML = elem.costo ? elem.costo : elem.costoDolares;
+        tdCostoViejo.innerHTML = elem.costo;
+        tdCostoDolaresViejo.innerHTML = elem.costodolar.toFixed(2);
         tdPrecioViejo.innerHTML = elem.precio_venta;
 
         tr.appendChild(tdCodigo);
         tr.appendChild(tdDescripcion);
         tr.appendChild(tdCostoViejo);
+        tr.appendChild(tdCostoDolaresViejo);
         tr.appendChild(tdPrecioViejo);
 
         tablaViejo.appendChild(tr);
@@ -87,16 +94,19 @@ const llenarListaNueva = async(lista)=>{
         const tdCodigo = document.createElement('td');
         const tdDescripcion = document.createElement('td');
         const tdCostoViejo = document.createElement('td');
+        const tdCostoDolaresViejo = document.createElement('td');
         const tdPrecioViejo = document.createElement('td');
 
         tdCodigo.innerHTML = elem._id;
         tdDescripcion.innerHTML = elem.descripcion;
-        tdCostoViejo.innerHTML = elem.costo ? elem.costo : elem.costoDolares;
-        tdPrecioViejo.innerHTML = elem.precio_venta;
+        tdCostoViejo.innerHTML = elem.costo.toFixed(2);
+        tdCostoDolaresViejo.innerHTML = elem.costodolar;
+        tdPrecioViejo.innerHTML = elem.precio_venta.toFixed(2);
 
         tr.appendChild(tdCodigo);
         tr.appendChild(tdDescripcion);
         tr.appendChild(tdCostoViejo);
+        tr.appendChild(tdCostoDolaresViejo);
         tr.appendChild(tdPrecioViejo);
 
         tablaNuevo.appendChild(tr);
