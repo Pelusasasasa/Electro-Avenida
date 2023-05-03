7ae3377cb2ee06a55b0eaeee13114a6236811098
Pelusa-not <agustinlorenzatto@gmail.com>
2023-05-03 11:29:17 -0300

    Estoy haciendo para que se muestren los detalles del prestamo

--- sistemaFacturacion/src/prestamos/verPrestamos.js
+++ sistemaFacturacion/src/prestamos/verPrestamos.js
@@ -6,6 +6,7 @@ const desde = document.getElementById('desde');
 const hasta = document.getElementById('hasta');
 
 const tbody = document.querySelector('tbody');
+const detallesProducto = document.querySelector('.detallesProducto');
 
 window.addEventListener('load',async e=>{
     const hoy = new Date();
@@ -16,6 +17,50 @@ window.addEventListener('load',async e=>{
     listarPrestamos(prestamos);
 });
 
-async function listarPrestamos(lista) {
-    console.log(lista)
+desde.addEventListener('keypress',traerPrestamos);
+hasta.addEventListener('keypress',traerPrestamos);
+
+tbody.addEventListener('click',mostrarDetalleProducto);
+
+async function traerPrestamos(e) {
+    if (e.keyCode === 13) {
+        const prestamos = (await axios.get(`${URL}prestamos/betweenDates/${desde.value}/${hasta.value}`)).data;
+        listarPrestamos(prestamos);
+
+        if (e.target.id === "desde") {
+            hasta.focus();
+        }
+    }
 };
+
+async function listarPrestamos(lista) {
+    for(let elem of lista){
+        console.log(elem)
+        const tr = document.createElement('tr');
+
+        const fecha = elem.fecha.slice(0,10).split('-',3);
+
+        const tdFecha = document.createElement('td');
+        const tdCodigo = document.createElement('td');
+        const tdCliente = document.createElement('td');
+        const tdNroComp = document.createElement('td');
+
+        tdFecha.innerText = `${fecha[2]}/${fecha[1]}/${fecha[0]}`;
+        tdCodigo.innerText = elem.codigo;
+        tdCliente.innerText = elem.cliente;
+        tdNroComp.innerText = elem.nro_comp;
+
+        tr.appendChild(tdFecha);
+        tr.appendChild(tdCodigo);
+        tr.appendChild(tdCliente);
+        tr.appendChild(tdNroComp);
+
+        tbody.appendChild(tr);
+
+        
+    }
+};
+
+async function mostrarDetalleProducto(e){
+    detallesProducto.classList.remove('none')
+}
