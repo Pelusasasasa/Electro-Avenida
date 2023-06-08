9d794869611dd8913cd6172aa27417f5f726aad3
Pelusa-not <agustinlorenzatto@gmail.com>
2023-06-08 11:44:50 -0300

    Sigo haceindo para que se pueda arreglar un saldo

--- sistemaFacturacion/src/clientes/arreglarCompensadaHistorica.js
+++ sistemaFacturacion/src/clientes/arreglarCompensadaHistorica.js
@@ -1,20 +1,18 @@
 const axios = require("axios");
-const { copiar, botonesSalir, redondear, cerrarVentana } = require("../funciones");
+const { copiar, botonesSalir, redondear, cerrarVentana, configAxios } = require("../funciones");
 require("dotenv").config;
 const URL = process.env.URL;
 
 const sweet = require('sweetalert2');
 
-const compensada = document.querySelector('.compensada');
-const historica = document.querySelector('.historica');
-
 const h1 = document.querySelector('h1');
-const labelImporte = document.querySelector('[for="importe"]');
-const labelPagado = document.querySelector('[for="pagado"]');
 
+
+const codigo = document.getElementById('codigo');
+const cliente = document.getElementById('cliente');
 const puntoVenta = document.getElementById('puntoVenta');
 const numero = document.getElementById('numero');
-const tbody = document.querySelector('tbody');
+
 
 let seleccionado;
 
@@ -22,10 +20,33 @@ window.addEventListener('click',e=>{
     // copiar();
     botonesSalir();
     cerrarVentana();
-})
+});
+
+codigo.addEventListener('change',traerCliente);
+
+puntoVenta.addEventListener('keypress',e=>{
+    if (e.keyCode === 13) {
+        numero.focus()
+    };
+});
 
 numero.addEventListener('change',async e=>{
     const nro_comp = puntoVenta.value.padStart(4,'0') + "-" + numero.value.padStart(8,'0');
-    cuentaTraida = (await axios.get(`${URL}cuentaComp/numero/${nro_comp}`)).data;
+    cuentaTraida = (await axios.get(`${URL}cuentaComp/numeroYCliente/${nro_comp}/${codigo.value.toUpperCase()}`)).data;
     console.log(cuentaTraida);
 });
+
+
+async function traerCliente(e) {
+    const clienteTraido = (await axios.get(`${URL}clientes/id/${codigo.value.toUpperCase()}`,configAxios)).data;   
+    if (clienteTraido) {
+        cliente.value = clienteTraido.cliente;
+        puntoVenta.focus();
+    }else{
+        sweet.fire({
+            title:"Cliente no encontrado"
+        });
+        codigo.value = "";
+    }
+    
+}
