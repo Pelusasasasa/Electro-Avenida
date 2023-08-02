9cb936bebdae87f89d1a61f24dac1c85c3a76ee0
Pelusa-not <agustinlorenzatto@gmail.com>
2023-08-02 12:57:44 -0300

    Arreglando un let en el servidor

--- servidor/backend/src/controllers/movCaja.controllers.js
+++ servidor/backend/src/controllers/movCaja.controllers.js
@@ -24,8 +24,9 @@ movCajaCTRL.getAll = async(req,res)=>{
 
 movCajaCTRL.getBetweenDates = async(req,res)=>{
     const {desde,hasta} = req.params;
+    let movimientos = [];
     try {
-        let movimientos = await MovCaja.find({
+        movimientos = await MovCaja.find({
             $and:[
                 {fecha:{$gte:new Date(desde + "T00:00:00.000Z")}},
                 {fecha:{$lte:new Date(hasta + "T23:59:59.000Z")}}
