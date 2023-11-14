764ec2dd0e4994d728201c436e9761c0f587a785
Pelusa-not <agustinlorenzatto@gmail.com>
2023-11-14 17:11:27 -0300

    Emepzando con la aplicacion para el telefono

--- AppElectroAvenida/src/components/Parrafo.jsx
+++
@@ -1,9 +0,0 @@
-const Parrafo = ({text,claseAdd,textAdicional}) => {
-  return (
-    <p className={claseAdd}>{textAdicional}
-      <span>{text ? text.toUpperCase() : ""}</span>
-    </p>
-  )
-}
-
-export default Parrafo
