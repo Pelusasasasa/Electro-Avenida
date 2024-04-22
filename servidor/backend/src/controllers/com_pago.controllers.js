9249aafe4aaf5320ff842cc43f7fb421c7872b35
Pelusa-not <agustinlorenzatto@gmail.com>
2024-04-22 11:51:06 -0300

    Hice para que me traiga el comprobante de pagos de la base de datos

--- servidor/backend/src/controllers/com_pago.controllers.js
+++ servidor/backend/src/controllers/com_pago.controllers.js
@@ -8,6 +8,12 @@ compPagoCTRL.getForCodProv = async (req, req) => {
     res.send(comprobantes)
 };
 
+compPagoCTRL.getForNumber = async (req, res) => {
+    const {number} = req.params;
+    const comprobante = await ComPago.findOne({number: number});
+    res.send(comprobante);
+};
+
 compPagoCTRL.post = async(req,res)=>{
     const now = new Date();
     req.body.fecha = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString();
