1858c51691d0a25d59bf74a77ef18c2969042d47
Pelusa-not <agustinlorenzatto@gmail.com>
2024-06-10 15:19:43 -0300

    Ticket puede imprimir ahora

--- sistemaFacturacion/src/impresion/ticket.js
+++ sistemaFacturacion/src/impresion/ticket.js
@@ -43,14 +43,14 @@ const cae = document.querySelector('.cae');
 const venciCae = document.querySelector('.venciCae');
 
 
- ipcRenderer.on('info-para-imprimir',async (e,args)=>{
-    [venta,afip,,movimientos,opciones] = JSON.parse(args);
+ipcRenderer.on('info-para-imprimir', async (e, args) => {
+    [venta, afip, , movimientos, opciones] = JSON.parse(args);
     let cliente;
 
     if (venta.tipo_comp === "Recibos") {
-        cliente = (await axios.get(`${URL}clientes/id/${venta.codigo}`,configAxios)).data;
-    }else{
-        cliente = (await axios.get(`${URL}clientes/id/${venta.cliente}`,configAxios)).data;
+        cliente = (await axios.get(`${URL}clientes/id/${venta.codigo}`, configAxios)).data;
+    } else {
+        cliente = (await axios.get(`${URL}clientes/id/${venta.cliente}`, configAxios)).data;
         cliente._id = venta.cliente;
         cliente.cliente = venta.nombreCliente;
         cliente.direccion = venta.direccion ? venta.direccion : cliente.direccion;
@@ -59,21 +59,21 @@ const venciCae = document.querySelector('.venciCae');
     };
     await infoComprobante(venta);
     await listarCliente(cliente);
-    movimientos ? await listaMovimientos(movimientos,venta) : await listar(venta,afip,opciones);
-    await listaIva(venta,afip,opciones);
+    movimientos ? await listaMovimientos(movimientos, venta) : await listar(venta, afip, opciones);
+    await listaIva(venta, afip, opciones);
     await listarAfip(afip);
-    // await ipcRenderer.send('imprimir',JSON.stringify(opciones));
- });
+    await ipcRenderer.send('imprimir', JSON.stringify(opciones));
+});
 
 async function infoComprobante(venta) {
     const now = new Date(venta.fecha);
     venta.fecha = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString();
     //fecha y hora
-    let date = venta.fecha.slice(0,10).split('-',3).reverse().join('/')
-    let time = venta.fecha.slice(11,19).split(':',3).join(':');
+    let date = venta.fecha.slice(0, 10).split('-', 3).reverse().join('/')
+    let time = venta.fecha.slice(11, 19).split(':', 3).join(':');
 
     const tipoFactura = verTipoFactura(venta.cod_comp)
-    codFactura.innerHTML = venta.cod_comp ?  "0"+venta.cod_comp : "06";
+    codFactura.innerHTML = venta.cod_comp ? "0" + venta.cod_comp : "06";
     tipo.innerHTML = tipoFactura;
     numeroFactura.innerHTML = venta.nro_comp;
 
@@ -96,26 +96,27 @@ async function infoComprobante(venta) {
     };
 }
 
-async function listar(venta,afip,opciones){
+async function listar(venta, afip, opciones) {
     if (venta.tipo_comp !== "Recibos") {
-        if(venta.productos){    
-            for await(let {objeto,cantidad} of venta.productos){
+        if (venta.productos) {
+            for await (let { objeto, cantidad } of venta.productos) {
                 const iva = objeto.iva === "N" ? 1.21 : 1.105;
                 const precio = objeto.oferta ? objeto.precioOferta : objeto.precio_venta;
                 listaProductos.innerHTML += `
                     <div class="cantidad">
-                        <p>${cantidad}/${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? (precio/iva).toFixed(2)  : precio}</p>
+                        <p>${cantidad}/${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? (precio / iva).toFixed(2) : precio}</p>
                         <p class=iva>${objeto.iva === "N" ? "(21.00)" : "(10.50)"}</p>
                         <p></p>
                     </div>
                     <div class="descripcionProducto">
-                        <p>${objeto.descripcion.slice(0,27)}</p>
-                        <p>${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? ((precio/iva)*cantidad).toFixed(2) : (precio * cantidad).toFixed(2)}</p>
+                        <p>${objeto.descripcion.slice(0, 27)}</p>
+                        <p>${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? ((precio / iva) * cantidad).toFixed(2) : (precio * cantidad).toFixed(2)}</p>
                     </div>
                 `
-        
-        }};
-    }else{
+
+            }
+        };
+    } else {
         cantidadPrecio.innerHTML = "";
         iva.innerHTML = "";
         descri.innerHTML = "Fecha";
@@ -123,7 +124,7 @@ async function listar(venta,afip,opciones){
         importe.innerHTML = "Pagado";
         observaciones.parentNode.classList.remove('none');
         observaciones.innerText = venta.observaciones;
-        for await(let producto of venta.comprobantes){
+        for await (let producto of venta.comprobantes) {
             listaProductos.innerHTML += `
                 <div class="cantidad  recibo">
                     <p>${producto.fecha}</p>
@@ -138,30 +139,30 @@ async function listar(venta,afip,opciones){
 };
 
 async function listaMovimientos(movimientos) {
-    
-    movimientos.map(({tipo_comp,iva,egreso,ingreso,descripcion,precio_unitario})=>{
+
+    movimientos.map(({ tipo_comp, iva, egreso, ingreso, descripcion, precio_unitario }) => {
         console.log(venta.condIva === "Monotributista")
         const ivaAux = iva === "N" ? 1.21 : 1.105;
         listaProductos.innerHTML += `
                     <div class="cantidad">
-                        <p>${tipo_comp === "Nota Credito" ? ingreso/ivaAux.toFixed(2) : egreso.toFixed(2)}
-                        /${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? (precio_unitario/ivaAux).toFixed(2)  : precio_unitario.toFixed(2)}</p>
+                        <p>${tipo_comp === "Nota Credito" ? ingreso / ivaAux.toFixed(2) : egreso.toFixed(2)}
+                        /${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? (precio_unitario / ivaAux).toFixed(2) : precio_unitario.toFixed(2)}</p>
                         <p class=iva>${iva === "N" ? "(21.00)" : "(10.50)"}</p>
                         <p></p>
                     </div>
                     <div class="descripcionProducto">
-                        <p>${descripcion.slice(0,27)}</p>
-                        <p>${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? ((precio_unitario/ivaAux)*egreso).toFixed(2) : (precio_unitario * egreso).toFixed(2)}</p>
+                        <p>${descripcion.slice(0, 27)}</p>
+                        <p>${(venta.condIva === "Inscripto" || venta.condIva === "Monotributista") ? ((precio_unitario / ivaAux) * egreso).toFixed(2) : (precio_unitario * egreso).toFixed(2)}</p>
                     </div>
                 `
     })
 };
 
 async function listarCliente(cliente) {
-       nombre.innerText = cliente.cliente;
-       cuit.innerText = cliente.cuit.length === 11 ? `CUIT: ${cliente.cuit}` : `DNI: ${cliente.cuit}`;
-       condIva.innerHTML = cliente.cond_iva.toUpperCase();
-       direccion.innerHTML = cliente.direccion + " - " + (venta.localidad ? venta.localidad : "CHAJARI");
+    nombre.innerText = cliente.cliente;
+    cuit.innerText = cliente.cuit.length === 11 ? `CUIT: ${cliente.cuit}` : `DNI: ${cliente.cuit}`;
+    condIva.innerHTML = cliente.cond_iva.toUpperCase();
+    direccion.innerHTML = cliente.direccion + " - " + (venta.localidad ? venta.localidad : "CHAJARI");
 };
 
 async function listarAfip(afip) {
@@ -169,12 +170,12 @@ async function listarAfip(afip) {
         qr.children[0].src = afip.QR;
         cae.innerHTML = afip.cae;
         venciCae.innerHTML = afip.vencimientoCae;
-    }else{
+    } else {
         divAfip.classList.add('none');
     }
 };
 
-async function listaIva(venta,afip,opciones){
+async function listaIva(venta, afip, opciones) {
     if ((venta.condIva === "Inscripto" || venta.condIva === "Monotributista") && venta.tipo_comp !== "Recibos") {
         if (venta.gravado21 !== 0) {
             discriminadorIva.innerHTML += `
@@ -203,20 +204,20 @@ async function listaIva(venta,afip,opciones){
     }
 }
 
-const verTipoFactura = (codigo)=>{
-     if (codigo === 6) {
-         return "Factura B";
-     }else if(codigo === 1){
-         return "Factura A";
-     }else if(codigo === 3){
-         return "Nota Credito A"
-     }else if(codigo === 8){
+const verTipoFactura = (codigo) => {
+    if (codigo === 6) {
+        return "Factura B";
+    } else if (codigo === 1) {
+        return "Factura A";
+    } else if (codigo === 3) {
+        return "Nota Credito A"
+    } else if (codigo === 8) {
         return "Nota Credito B"
-    }else if(codigo === 4){
+    } else if (codigo === 4) {
         return "Recibos"
-    }else if(codigo === 9){
+    } else if (codigo === 9) {
         return "Recibos"
-    }else{
+    } else {
         return "Comprobante"
     }
 };
