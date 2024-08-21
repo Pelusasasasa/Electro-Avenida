0346df759906837c91ccb4263bef3eaecdb7f2f2
Pelusa-not <agustinlorenzatto@gmail.com>
2024-08-21 11:50:09 -0300

    Reacomode el codigo

--- servidor/backend/src/app.js
+++ servidor/backend/src/app.js
@@ -21,51 +21,53 @@ app.use('/api/cheques',require('./routes/cheques'));
 app.use('/api/clientes',require('./routes/clientes'));
 //comprobante de pago
 app.use('/api/compPagos',require('./routes/comp_pago'));
-
-app.use('/api/usuarios',require('./routes/usuarios'))
-
-app.use('/api/pedidos',require('./routes/pedidos'))
-app.use('/api/productos',require('./routes/productos'))
-app.use('/api/tipoVenta',require('./routes/tipoVenta'))
-app.use('/api/movProductos',require('./routes/movProductos'))
-
-app.use('/api/ventas',require('./routes/ventas'))
-//Presupuesto
-app.use('/api/presupuesto',require('./routes/presupuesto'))
-
-
-app.use('/api/cuentaComp',require('./routes/cueCorrComp'));
-app.use('/api/cuentaHisto',require('./routes/cueCorrHist'));
-
 //Imprimir un pdf
 app.use('/api/crearPdf',require('./routes/pdf'));
 //tarjetas
-app.use('/api/tarjetas',require('./routes/tarjetas'));
-//tipo Tarjetas
 app.use('/api/tipoTarjetas',require('./routes/tipoTarjetas'));
-//vales
-app.use('/api/vales',require('./routes/vale'));
-//cuentas
-app.use('/api/cuentas',require('./routes/cuenta'));
-//movCaja
-app.use('/api/movCajas',require('./routes/movCaja'));
-
-//Ultimos
-app.use('/api/ultimos',require('./routes/ultimos'));
-//Provedor
-app.use('/api/provedor',require('./routes/provedor'));
-//datos Comprobantes
-app.use('/api/dat_comp',require('./routes/dat_comp'));
-//rubros
-app.use('/api/rubros',require('./routes/rubros'));
 //Cuenta Corriente provedor
 app.use('/api/ctactePro',require('./routes/ctatePro'));
+//Cuenta Corriente Compensada
+app.use('/api/cuentaComp',require('./routes/cueCorrComp'));
+//Cuenta Corriente Historica
+app.use('/api/cuentaHisto',require('./routes/cueCorrHist'));
+//cuentas
+app.use('/api/cuentas',require('./routes/cuenta'));
+//datos Comprobantes
+app.use('/api/dat_comp',require('./routes/dat_comp'));
+//Diferencia de caja
+app.use('/api/difCaja',require('./routes/difCaja'));
+//movCaja
+app.use('/api/movCajas',require('./routes/movCaja'));
+//Movimiento de productos
+app.use('/api/movProductos',require('./routes/movProductos'))
+//Pedidos
+app.use('/api/pedidos',require('./routes/pedidos'));
+//Prestamos
+app.use('/api/prestamos',require('./routes/prestamo'));
+//Presupuesto
+app.use('/api/presupuesto',require('./routes/presupuesto'))
+//Productos
+app.use('/api/productos',require('./routes/productos'))
+//Provedor
+app.use('/api/provedor',require('./routes/provedor'));
 //Recibos
 app.use('/api/recibos',require('./routes/recibo'));
 //Remitos
 app.use('/api/remitos',require('./routes/remito'));
-//Prestamos
-app.use('/api/prestamos',require('./routes/prestamo'));
-//Diferencia de caja
-app.use('/api/difCaja',require('./routes/difCaja'));
+//rubros
+app.use('/api/rubros',require('./routes/rubros'));
+//tipo Tarjetas
+app.use('/api/tarjetas',require('./routes/tarjetas'));
+//Tipo Ventas
+app.use('/api/tipoVenta',require('./routes/tipoVenta'));
+//Ultimos
+app.use('/api/ultimos',require('./routes/ultimos'));
+//Usuarios
+app.use('/api/usuarios',require('./routes/usuarios'))
+//Vales
+app.use('/api/vales',require('./routes/vale'));
+//Ventas
+app.use('/api/ventas',require('./routes/ventas'))
+
 module.exports = app
--- servidor/backend/src/controllers/ventas.controllers.js
+++ servidor/backend/src/controllers/ventas.controllers.js
@@ -11,36 +11,14 @@ ventasCTRL.cargarVenta = async(req,res)=>{
     venta.save()
     console.log(`Venta ${req.body.nro_comp} guardada por el vendedor ${req.body.vendedor} en la maquina ${req.body.maquina} con la hora ${(new Date()).toLocaleString()}`);
     res.send(venta)
-}
+};
 
-ventasCTRL.traerVentas = async(req,res)=>{
+ventasCTRL.eliminarVenta = async(req,res)=>{
     const {id} = req.params;
-    const venta = await Ventas.find();
-    res.send(venta)
-}
-
-ventasCTRL.traerVentaUnica = async(req,res)=>{
-    const {numero,tipo} = req.params;
-    let tipo_comp = "";
-    let venta;
-    if (tipo === "6" || tipo === "1") {
-        tipo_comp = "Ticket Factura";
-        venta = await Ventas.find({nro_comp:numero,tipo_comp:tipo_comp,cod_comp:tipo});
-    }else if(tipo === "3" || tipo === "8"){
-        tipo_comp = "Nota Credito";
-        venta = await Ventas.find({nro_comp:numero,tipo_comp:tipo_comp,cod_comp:tipo});
-    }else{
-        venta = await Ventas.find({nro_comp:numero,tipo_comp:tipo});
-    }
-    res.send(venta[0]);
-}
-
-ventasCTRL.modificarVentas = async(req,res) =>{
-    const {id} = req.params;
-    await Ventas.findByIdAndUpdate({_id:id},req.body)
-    console.log(`Venta ${req.body.nro_comp} Modificada`)
-    res.send("Venta Modificada")
-}
+    const a = await Ventas.findOneAndDelete({nro_comp:id}); 
+    console.log(`Venta ${id} Eliminada`);
+    res.send(a);
+};
 
 ventasCTRL.entreFechas = async(req,res) => {
     const {desde,hasta} = req.params;
@@ -52,7 +30,7 @@ ventasCTRL.entreFechas = async(req,res) => {
         ]
     });
     res.send(ventas)
-}
+};
 
 ventasCTRL.entreFechasConId = async(req,res) => {
     const {id,desde,hasta} = req.params
@@ -64,25 +42,19 @@ ventasCTRL.entreFechasConId = async(req,res) => {
             {fecha:{$lte: new Date(hasta + "T23:59:59.00Z")}}
         ]})
     res.send(ventaARetornar)
-}
+};
 
 ventasCTRL.entreFechasConCliente = async(req,res) => {
     const {idCliente,desde,hasta} = req.params
     const ventaARetornar =  await Ventas.find({$and:[{cliente:idCliente},{fecha:{$gte: new Date(desde)}},{fecha:{$lte: new Date(hasta)}}]})
     res.send(ventaARetornar)
-}
+};
 
-ventasCTRL.traerTicket = async(req,res)=>{
-    const {numero,tipo,condIva} = req.params;
-    const venta = (await Ventas.findOne({nro_comp:numero,tipo_comp:tipo,condIva:condIva}));
-    res.send(venta)
-}
-
-ventasCTRL.eliminarVenta = async(req,res)=>{
+ventasCTRL.modificarVentas = async(req,res) =>{
     const {id} = req.params;
-    const a = await Ventas.findOneAndDelete({nro_comp:id}); 
-    console.log(`Venta ${id} Eliminada`);
-    res.send(a);
+    await Ventas.findByIdAndUpdate({_id:id},req.body)
+    console.log(`Venta ${req.body.nro_comp} Modificada`)
+    res.send("Venta Modificada")
 };
 
 ventasCTRL.getVentasForMonth = async(req,res)=>{
@@ -100,6 +72,39 @@ ventasCTRL.getVentasForMonth = async(req,res)=>{
     res.send(ventas);
 
 
+};
+
+ventasCTRL.traerTicket = async(req,res)=>{
+    const {numero,tipo,condIva} = req.params;
+    const venta = (await Ventas.findOne(
+        {
+            nro_comp:numero,
+            tipo_comp:tipo,
+            condIva:condIva
+        }));
+    res.send(venta)
+};
+
+ventasCTRL.traerVentas = async(req,res)=>{
+    const {id} = req.params;
+    const venta = await Ventas.find();
+    res.send(venta)
+};
+
+ventasCTRL.traerVentaUnica = async(req,res)=>{
+    const {numero,tipo} = req.params;
+    let tipo_comp = "";
+    let venta;
+    if (tipo === "6" || tipo === "1") {
+        tipo_comp = "Ticket Factura";
+        venta = await Ventas.find({nro_comp:numero,tipo_comp:tipo_comp,cod_comp:tipo});
+    }else if(tipo === "3" || tipo === "8"){
+        tipo_comp = "Nota Credito";
+        venta = await Ventas.find({nro_comp:numero,tipo_comp:tipo_comp,cod_comp:tipo});
+    }else{
+        venta = await Ventas.find({nro_comp:numero,tipo_comp:tipo});
+    }
+    res.send(venta[0]);
 };
 
 module.exports = ventasCTRL;
