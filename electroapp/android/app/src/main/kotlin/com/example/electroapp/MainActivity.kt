5de4606186545fcda17c04fd3e6fa005c8272477
Pelusa <agustinlorenzatto@gmail.com>
2024-11-26 17:53:17 -0300

    Borrando carpetas inecesarias

--- electroapp/android/app/src/main/kotlin/com/example/electroapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.electroapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- electroapp/lib/src/widgets/custom_widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'package:electroapp/src/widgets/custom_appBar.dart';
-export 'package:electroapp/src/widgets/producto_img.dart';
-export 'package:electroapp/src/widgets/producto_info.dart';
