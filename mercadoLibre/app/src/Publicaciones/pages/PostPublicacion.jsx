0e3ca74e6a7ab08073d75efe00ccfa25324eea1b
Pelusa <agustinlorenzatto@gmail.com>
2024-11-28 18:53:53 -0300

    Agregue varias caracteristicas

--- mercadoLibre/app/src/Publicaciones/pages/PostPublicacion.jsx
+++ mercadoLibre/app/src/Publicaciones/pages/PostPublicacion.jsx
@@ -16,6 +16,8 @@ const initialForm = {
     stock: 0,
     categories: 'MLA1574',
     subCategories: 'MLA1582',
+    voltaje: '220V',
+    temperaturaLuz: '',
     imagenes: []
 }
 
@@ -23,7 +25,11 @@ export const PostPublicacion = () => {
     const { active } = useSelector(state => state.productos);
     const dispatch = useDispatch()
 
-    const {onInputChange, codigo, descripcion, marca, costoIva, imagenes, precioSujerido, stockSujerido, precio, stock, categories, subCategories, subCategories1, formState ,onChanges} = useForm(initialForm);
+    const {
+         subCategories, subCategories1, voltaje, temperaturaLuz, colorLuz, potencia, tipofuente, voltaje2, formState ,onChanges,
+        onInputChange, codigo, descripcion, marca, costoIva, imagenes, precioSujerido, stockSujerido, precio, stock, categories,
+        tipoBateria, formato, forma, lugarMontaje, material, ambiente, capacidadFoco
+        } = useForm(initialForm);
 
     const [categorias, setCategorias] = useState([]);
     const [subCategorias, setSubCategorias] = useState([]);
@@ -95,7 +101,7 @@ export const PostPublicacion = () => {
     const agregar = async(e) => {
         e.preventDefault();
         dispatch(saved());
-        console.log(new URL(imagenes))
+        console.log(potencia)
 
 
          let producto = {};
@@ -132,27 +138,80 @@ export const PostPublicacion = () => {
 
          producto.attributes = [
              {
+                 id: 'AMBIENTS',
+                 value_name: ambiente
+             },
+             {
+                 id: 'BATTERY_TYPE',
+                 value_name: tipoBateria
+             },
+             {
                  id: 'BRAND',
                  value_name: formState.marca
+             },
+             {
+                id: 'COLOR_TEMPERATURE',
+                value_name: temperaturaLuz
+             },
+             {
+                 id: 'MATERIALS',
+                 value_name: material
              },
              {
                  id: 'MODEL',
                  value_name: active.cod_fabrica
              },
              {
-            "id": "SELLER_SKU",
-            "name": "SKU",
-            "value_id": null,
-            "value_name": `${codigo}`,
-            "values": [
-                {
-                    "id": null,
-                    "name": `${codigo}`,
-                    "struct": null
-                }
-            ],
-            "value_type": "string"
-        },
+                 id: 'MOUNTING_PLACES',
+                 value_name: lugarMontaje
+             },
+             {
+                 id: 'LAMP_FORMAT',
+                 value_id: formato
+             },
+             {
+                id: 'LIGHT_BULBS_CAPACITY',
+                value_name: capacidadFoco
+             },
+             {
+                id: 'LIGHT_COLOR',
+                value_name: colorLuz
+             },
+             {
+                id: 'LIGHT_SOURCES_TYPES',
+                value_name: tipofuente
+             },
+             {
+                id: 'SHAPE',
+                value_name: forma
+             },
+             {
+                "id": "SELLER_SKU",
+                "name": "SKU",
+                "value_id": null,
+                "value_name": `${codigo}`,
+                "values": [
+                    {
+                        "id": null,
+                        "name": `${codigo}`,
+                        "struct": null
+                    }
+                ],
+                "value_type": "string"
+            },
+            {
+                id: 'POWER',
+                value_name: potencia ? potencia + ' W' : null,
+                value_id: potencia ? null : "-1"
+            },
+            {
+                id: 'POWER_SUPPLY_TYPE',
+                value_id: voltaje2 
+            },
+            {
+                id: 'VOLTAGE',
+                value_name: voltaje
+            }
          ]
 
          dispatch( postPublicaciones(producto) )
@@ -190,21 +249,26 @@ export const PostPublicacion = () => {
                 <label htmlFor="stockSujerido" className='text-center font-bold '>Stock Sujerido</label>
                 <input type="number" name="stockSujerido" id="stockSujerido" className='bg-gray-300' disabled value={stockSujerido}/>
             </div>
+
         </section>
 
-        <section className='grid grid-cols-3 gap-3 m-2'>
+        <section id='interno' className='grid grid-cols-3 gap-3 m-2'>
+            
             <div className='flex flex-col'>
                 <label htmlFor="imagenes" className='text-center font-bold '>Imagenes</label>
                 <input type="file" multiple accept='image/*' name="imagenes" id="imagenes" className='bg-white' value={imagenes} onChange={onInputChange}/>
             </div>
+
             <div className='flex flex-col'>
                 <label htmlFor="precio" className='text-center font-bold '>Precio</label>
                 <input type="number" name="precio" id="precio" value={precio} onChange={onInputChange} className='bg-white'/>
             </div>
+
             <div className='flex flex-col'>
                 <label htmlFor="stock" className='text-center font-bold '>Stock</label>
                 <input type="number" name="stock" id="stock" value={stock} onChange={onInputChange} className='bg-white'/>
             </div>
+
         </section>
 
         <section id='interno' className='grid grid-cols-4 gap-3 m-2'>
@@ -243,6 +307,92 @@ export const PostPublicacion = () => {
                     ))}
                 </select>
             </div>
+            
+            <div className='flex flex-col'>
+                <label htmlFor="voltaje" className='text-center font-bold '>Voltaje</label>
+                <select name="voltaje" id="voltaje" value={voltaje} onChange={onInputChange}>
+                    <option value="220V">220V</option>
+                    <option value="12V">12V</option>
+                </select>
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="colorLuz" className='text-center font-bold '>Color Luz</label>
+                <select name="colorLuz" id="colorLuz" value={colorLuz} onChange={onInputChange}>
+                    <option value="null">N/A</option>
+                    <option value="BLANCO">BLANCO</option>
+                    <option value="CALIDO">CALIDO</option>
+                    <option value="NEUTRO">NEUTRO</option>
+                </select>
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="temperaturaLuz" className='text-center font-bold '>Temperatura Luz</label>
+                <select name="temperaturaLuz" id="temperaturaLuz" value={temperaturaLuz} onChange={onInputChange}>
+                    <option value="null">N/A</option>
+                    <option value="6500 K">6500 K</option>
+                    <option value="3000 K">3000 K</option>
+                    <option value="4000 K">4000 K</option>
+                </select>
+            </div>
+            
+            <div className='flex flex-col'>
+                <label htmlFor="potencia" className='text-center font-bold '>Potencia</label>
+                <input type="number" name="potencia" id="potencia" onChange={onInputChange} value={potencia} />
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="tipofuente" className='text-center font-bold '>Tipo Fuente</label>
+                <select name="tipofuente" id="tipofuente" value={tipofuente} onChange={onInputChange}>
+                    <option value="7387210">LED</option>
+                    <option value="3137301">INCANDESENTE</option>
+                </select>
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="voltaje2" className='text-center font-bold '>Voltaje 2</label>
+                <select name="voltaje2" id="voltaje2" value={voltaje2} onChange={onInputChange}>
+                    <option value="220V">220V</option>
+                    <option value="12V">12V</option>
+                </select>
+            </div>
+
+            
+            <div className='flex flex-col'>
+                <label htmlFor="formato" className='text-center font-bold '>Formato de la lampara</label>
+                <select name="formato" id="formato" value={formato} onChange={onInputChange}>
+                    <option value="null">N/A</option>
+                    <option value="13435240">Colgante</option>
+                    <option value="13685639">Aplique</option>
+                </select>
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="forma" className='text-center font-bold '>Forma</label>
+                <input type="text" name="forma" id="forma" onChange={onInputChange} value={forma} />
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="lugarMontaje" className='text-center font-bold '>Lugares de montaje</label>
+                <input type="text" name="lugarMontaje" id="lugarMontaje" onChange={onInputChange} value={lugarMontaje} />
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="material" className='text-center font-bold '>Material</label>
+                <input type="text" name="material" id="material" onChange={onInputChange} value={material} />
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="ambiente" className='text-center font-bold '>Ambiente</label>
+                <input type="text" name="ambiente" id="ambiente" onChange={onInputChange} value={ambiente} />
+            </div>
+
+            <div className='flex flex-col'>
+                <label htmlFor="capacidadFoco" className='text-center font-bold '>Capacidad Foco</label>
+                <input type="number" name="capacidadFoco" id="capacidadFoco" onChange={onInputChange} value={capacidadFoco} />
+            </div>
+            
+
             
         </section>
 
