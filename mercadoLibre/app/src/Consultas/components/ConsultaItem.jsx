e4b399c902161c3f26cc84214bca91828c675fb9
Pelusa <agustinlorenzatto@gmail.com>
2024-12-06 09:01:39 -0300

    Listamos las consultas que hagan

--- mercadoLibre/app/src/Consultas/components/ConsultaItem.jsx
+++ mercadoLibre/app/src/Consultas/components/ConsultaItem.jsx
@@ -13,13 +13,20 @@ export const ConsultaItem = ({id, date_created, text, status}) => {
 
         
     }
+    let estado = '';
+
+    if (status === 'ANSWERED'){
+        estado = 'RESPONDIDO';
+    }else if (status === 'UNANSWERED'){
+        estado = 'NO RESPONDIDO';
+    };
 
   return (
-    <tr className={`cursor-pointer ${active.id === id ? 'bg-blue-300' : 'hover:bg-gray-400'}`}>
-        <td className='border border-black'>{date_created.slice(0, 10).split('-', 3).reverse().join('/')}</td>
-        <td className='border border-black'>{text}</td>
-        <td className='border border-black'>{status}</td>
-        <td className="border border-black">
+    <tr className={`cursor-pointer ${active.id === id ? 'bg-blue-300' :  status === 'ANSWERED' ? 'hover:bg-green-400' : 'hover:bg-red-400'} }`}>
+        <td className={`border ${status === 'ANSWERED' ? 'border-green-500' : 'border-red-400'}`}>{date_created.slice(0, 10).split('-', 3).reverse().join('/')}</td>
+        <td className={`border ${status === 'ANSWERED' ? 'border-green-500' : 'border-red-400'}`}>{text}</td>
+        <td className={`border ${status === 'ANSWERED' ? 'border-green-500' : 'border-red-400'}`}>{estado}</td>
+        <td className={`border ${status === 'ANSWERED' ? 'border-green-500' : 'border-red-400'}`}>
             <button className="w-full bg-gray-500 text-white font-bold"  onClick={responder}>Responder</button>
         </td>
     </tr>
--- mercadoLibre/app/src/Consultas/pages/ListaConsulta.jsx
+++ mercadoLibre/app/src/Consultas/pages/ListaConsulta.jsx
@@ -2,83 +2,97 @@ import React from 'react';
 import { ConsultaItem } from '../components/ConsultaItem';
 
 const res = {
-      "total": 1,
-      "limit": 50,
-      "questions": [
-          {
-              "date_created": "2024-12-04T15:58:18.103-04:00",
-              "item_id": "MLA1966788456",
-              "seller_id": 231090073,
-              "status": "ANSWERED",
-              "text": "Hola, cuantas necesito para iluminar una habitación de 10x6metros",
-              "tags": [],
-              "id": 13221614379,
-              "deleted_from_listing": false,
-              "hold": false,
-              "answer": {
-                  "text": "Hola, Podes convinar en 2 hileras 3 colgantes y luego trendias que usar focos de una buenta intecidad",
-                  "status": "ACTIVE",
-                  "date_created": "2024-12-04T16:01:55.992-04:00"
-              },
-              "from": {
-                  "id": 314921953
-              }
-          }
-      ],
-      "filters": {
-          "limit": 50,
-          "offset": 0,
-          "api_version": "4",
-          "is_admin": false,
-          "sorts": [],
-          "caller": 231090073,
-          "seller": 231090073,
-          "client_id": 8351426981367452
-      },
-      "available_filters": [
-          {
-              "id": "item",
-              "name": "Item",
-              "type": "text"
-          },
-          {
-              "id": "from",
-              "name": "From user id",
-              "type": "number"
-          },
-          {
-              "id": "totalDivisions",
-              "name": "total divisions",
-              "type": "number"
-          },
-          {
-              "id": "division",
-              "name": "Division",
-              "type": "number"
-          },
-          {
-              "id": "status",
-              "name": "Status",
-              "type": "text",
-              "values": [
-                  "ANSWERED",
-                  "BANNED",
-                  "CLOSED_UNANSWERED",
-                  "DELETED",
-                  "DISABLED",
-                  "UNANSWERED",
-                  "UNDER_REVIEW"
-              ]
-          }
-      ],
-      "available_sorts": [
-          "item_id",
-          "from_id",
-          "date_created",
-          "seller_id"
-      ]
+    "total": 2,
+    "limit": 50,
+    "questions": [
+        {
+            "date_created": "2024-12-04T15:58:18.103-04:00",
+            "item_id": "MLA1966788456",
+            "seller_id": 231090073,
+            "status": "ANSWERED",
+            "text": "Hola, cuantas necesito para iluminar una habitación de 10x6metros",
+            "tags": [],
+            "id": 13221614379,
+            "deleted_from_listing": false,
+            "hold": false,
+            "answer": {
+                "text": "Hola, Podes convinar en 2 hileras 3 colgantes y luego trendias que usar focos de una buenta intecidad",
+                "status": "ACTIVE",
+                "date_created": "2024-12-04T16:01:55.992-04:00"
+            },
+            "from": {
+                "id": 314921953
+            }
+        },
+        {
+            "date_created": "2024-12-05T09:31:17.001-04:00",
+            "item_id": "MLA1458728773",
+            "seller_id": 231090073,
+            "status": "UNANSWERED",
+            "text": "Solo para uso interiror?",
+            "tags": [],
+            "id": 13222163133,
+            "deleted_from_listing": false,
+            "hold": false,
+            "answer": null,
+            "from": {
+                "id": 486716672
+            }
+        }
+    ],
+    "filters": {
+        "limit": 50,
+        "offset": 0,
+        "api_version": "4",
+        "is_admin": false,
+        "sorts": [],
+        "caller": 231090073,
+        "seller": 231090073,
+        "client_id": 8351426981367452
+    },
+    "available_filters": [
+        {
+            "id": "item",
+            "name": "Item",
+            "type": "text"
+        },
+        {
+            "id": "from",
+            "name": "From user id",
+            "type": "number"
+        },
+        {
+            "id": "totalDivisions",
+            "name": "total divisions",
+            "type": "number"
+        },
+        {
+            "id": "division",
+            "name": "Division",
+            "type": "number"
+        },
+        {
+            "id": "status",
+            "name": "Status",
+            "type": "text",
+            "values": [
+                "ANSWERED",
+                "BANNED",
+                "CLOSED_UNANSWERED",
+                "DELETED",
+                "DISABLED",
+                "UNANSWERED",
+                "UNDER_REVIEW"
+            ]
+        }
+    ],
+    "available_sorts": [
+        "item_id",
+        "from_id",
+        "date_created",
+        "seller_id"
+    ]
 };
-
 
 export const ListaConsulta = () => {
   console.log(res.questions[0])
