253d5730f9989c43a261772379f65be730a26185
Pelusa <agustinlorenzatto@gmail.com>
2024-12-26 08:13:12 -0300

    Mejorando el sistema

--- mercadoLibre/app/src/MercadoApp.jsx
+++ mercadoLibre/app/src/MercadoApp.jsx
@@ -2,8 +2,15 @@ import React, { useEffect } from 'react'
 import { AppRouter } from './router/AppRouter'
 import { AsideBar } from './components/AsideBar'
 import { verificarToken } from './helpers/funciones'
+import { useVariableStore } from './hooks/useVariableStore'
 
 export const MercadoApp = () => {
+
+  const { startGetVariables } = useVariableStore();
+
+  useEffect(() => {
+    startGetVariables()
+  }, [])
 
   useEffect(() => {
     verificarToken()
