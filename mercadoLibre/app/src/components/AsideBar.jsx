0c75e2518f7a588f3bd4f5eb909f41381570e57f
Pelusa-not <agustinlorenzatto@gmail.com>
2025-01-03 14:15:49 -0300

    Poniendo una alerta para saber si hay consultas

--- mercadoLibre/app/src/components/AsideBar.jsx
+++ mercadoLibre/app/src/components/AsideBar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AsideItem } from './AsideItem'
+import { useSelector } from 'react-redux';
 
 export const AsideBar = () => {
+
+  const [sinResponder, setSinResponder] = useState(0);
+  const { consultas } = useSelector(state => state.consultas);
+
+  useEffect(() => {
+    setSinResponder(consultas.filter(consulta => consulta.status !== 'ANSWERED').length);
+  }, [consultas]);
+
   return (
     <aside className='w1/4 bg-yellow-400 text-gray-800 font-bold p-2'>
         <ul>
             <AsideItem path='/publicaciones/list' text='Publicaciones'/>
             <AsideItem path='/ventas/list' text='Ventas' />
-            <AsideItem path='/consultas/list' text='Consultas' />
+
+            <div className='flex relative'>
+              <div className='w-full'>
+                <AsideItem path='/consultas/list' text='Consultas' />
+              </div>
+              <p className='absolute right-0 bg-red-400 border border-black rounded-3xl p-2'>{sinResponder}</p>
+            </div>
         </ul>
     </aside>
   )
