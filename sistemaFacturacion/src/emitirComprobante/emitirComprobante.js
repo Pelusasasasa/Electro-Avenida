7db402b290a49bb2cafdf3157c5bb9b982b7bd6d
Pelusa <agustinlorenzatto@gmail.com>
2025-08-06 12:08:42 -0300

    Ahora los movimientos de Remitos no desceuntan el stock en el listado

--- sistemaFacturacion/src/emitirComprobante/emitirComprobante.js
+++ sistemaFacturacion/src/emitirComprobante/emitirComprobante.js
@@ -641,7 +641,7 @@ async function movimientoProducto(cantidad, objeto,idCliente, cliente, tipo_pago
   movProducto.nro_comp = nro_comp;
   movProducto.egreso = cantidad;
   movProducto.iva = objeto.iva;
-  movProducto.stock = tipo_pago === "PP" ? objeto.stock : parseFloat((parseFloat(objeto.stock) - cantidad).toFixed(2));
+  movProducto.stock = (tipo_pago === "PP" || tipo_pago === 'RT') ? objeto.stock : parseFloat((parseFloat(objeto.stock) - cantidad).toFixed(2));
   movProducto.precio_unitario = objeto.oferta ? objeto.precioOferta : parseFloat(objeto.precio_venta);
   movProducto.total = (parseFloat(movProducto.egreso) * parseFloat(movProducto.precio_unitario)).toFixed(2);
   movProducto.rubro = objeto.rubro;
