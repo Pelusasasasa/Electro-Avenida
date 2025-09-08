bfa35409c9ddc13af118a0146af51a144d00a006
Pelusa-not <agustinlorenzatto@outlook.com>
2025-09-08 11:07:46 -0300

    Haciendo el commit

--- caja/src/provedores/emitirPago.js
+++ caja/src/provedores/emitirPago.js
@@ -136,17 +136,11 @@ const agregarCheque = () => {
 
 const cambiarNumeroComprobantePago = async(lista) => {
   for await (let elem of lista) {
-    if (
-      elem.children[1].innerText !== "Pago Anticipado" &&
-      elem.children[1].innerText !== "Pago A Cuenta"
-    ) {
-      const comprobante = (
-        await axios.get(
-          `${URL}ctactePro/numero/${elem.children[0].innerText}`,
-          configAxios
-        )
-      ).data;
+    if (elem.children[1].innerText !== "Pago Anticipado" && elem.children[1].innerText !== "Pago A Cuenta") {
+      const comprobante = (await axios.get(`${URL}ctactePro/numero/${elem.children[0].innerText}`)).data;
+
       comprobante.com_pago = numeroVenta.value;
+      
       await axios.put(
         `${URL}ctactePro/id/${comprobante._id}`,
         comprobante,
